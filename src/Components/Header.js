b14b5dd6f35afdbe5d613cf9656ab06e7eb66ee3
Sergey <90088934+093874413sergey@users.noreply.github.com>
2022-11-04 18:37:38 +0200

    Added registration

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsCart4 } from "react-icons/bs";
 import { GoSignIn } from "react-icons/go";
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 export default function Header(props) {
+  const [cartOpen, setCartOpen] = useState(false)
   return (
     <header className="block">
       <div>
@@ -17,7 +21,31 @@ export default function Header(props) {
             ''
           )}
         </a>{' '}
-        <a href="#/signin"> <GoSignIn className='button_signin' /></a>
+        <GoSignIn onClick={() => setCartOpen(!cartOpen)} className={`button_signin`} />
+      {cartOpen && (
+        <div className='blok_reg'>
+          <Form className='form'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control className='email' type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control className='password' type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button className='button_submit' variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+        </div>
+      )}
       </div>
     </header>
   );
