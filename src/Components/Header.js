45ec5f9332c281a2c867148febe7e7fd99cc2f16
Sergey <90088934+093874413sergey@users.noreply.github.com>
2022-11-04 18:53:13 +0200

    add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { BsCart4 } from "react-icons/bs";
 import { GoSignIn } from "react-icons/go";
-
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -24,26 +23,26 @@ export default function Header(props) {
         <GoSignIn onClick={() => setCartOpen(!cartOpen)} className={`button_signin`} />
       {cartOpen && (
         <div className='blok_reg'>
-          <Form className='form'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control className='email' type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+                <Form className='form'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control className='email' type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control className='password' type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button className='button_submit' variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control className='password' type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button className='button_submit' variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </div>
       )}
       </div>
